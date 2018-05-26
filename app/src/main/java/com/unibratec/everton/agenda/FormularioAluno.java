60729845a255c97ffac0b5aafd9b625bc97076ef
Everton Walker <everton.walker@hotmail.com>
2018-05-26 15:59:20 -0300

    adicionando formulário de cadastro de aluno

---
+++ app/src/main/java/com/unibratec/everton/agenda/FormularioAluno.java
@@ -0,0 +1,27 @@
+package com.unibratec.everton.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+
+        Button botaoSalvar = (Button) findViewById(R.id.botao_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioAluno.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
